0fafed86d845b53225b8e334614aecd82daaeb94
liubovtolka4eva-collab <liubovtolka4eva@gmail.com>
2026-03-18 21:00:38 +0300

    fix syntax

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import os, random, string, qrcode, io, base64
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lovespace-secret-key-2024'
 import os
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///lovespace.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///lovespace.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
