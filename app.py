39e0ba22c816febd00512fac501a55822dc0a429
tanyapark498-ui <tanyapark498@gmail.com>
2026-04-22 10:36:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-```python
+
 from flask import Flask, render_template, request, jsonify, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
